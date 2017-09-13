a1b8e8c04eb357be5414f8f95d9a1b61a06bd0e4
DESKTOP-3FMRHL7\kuban <502538331@qq.com>
2017-09-13 21:16:13 +0800

    fix

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
+var jsBridge
+
+var clientWidth = document.documentElement.clientWidth
+document.documentElement.style.fontSize = clientWidth/3.75 + "px";
+window.onresize = function(){
+    clientWidth = document.documentElement.clientWidth
+    document.documentElement.style.fontSize = clientWidth/3.75 + "px";
+}
+
 function setupKBWebviewJSBridge(callback) {
     if (window.KBWebviewAndroidJSBridge) {
         callback(KBWebviewAndroidJSBridge)
@@ -46,20 +55,29 @@ setupKBWebviewJSBridge(function(bridge) {
     }
 
     bridge.registerHandler('getUserInfo', function(data, responseCallback) {
+        var responseData = { 'Javascript Says':'Right back atcha!','name':'whe' }
+        data_wangxuan = data
+        document.getElementById('info').innerHTML = data
         responseCallback({"userID":"DX001", "userName":"旋之华", "age":"18", "otherName":"旋之华"})
     })
+
+    document.body.appendChild(document.createElement('br'))
     jsBridge=bridge;
 })
-
-function toNews(id) {
-    jsBridge.callHandler(
-        'newsDetail'
-        , '/details.html?news_id=' + id
-    );
-}
 
 function getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
     var r = window.location.search.substr(1).match(reg);  //匹配目标参数
     if (r != null) return unescape(r[2]); return null; //返回参数值
+}
+
+function callClientHandler (url, data, type) {
+    jsBridge.callHandler(
+        'callClientHandler',
+        {
+            url : url,
+            type : type,
+            data : data
+        }
+    );
 }
